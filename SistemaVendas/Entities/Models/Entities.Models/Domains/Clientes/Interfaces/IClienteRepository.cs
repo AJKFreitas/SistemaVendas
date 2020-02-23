c981531406e8cda86974207a08dccbc111b1ab72
ajkfreitas <ajkfreitas@gmail.com>
2020-02-23 20:44:03 -0300

    merge clietes

--- SistemaVendas/Entities/Models/Entities.Models/Domains/Clientes/Interfaces/IClienteRepository.cs
+++ SistemaVendas/Entities/Models/Entities.Models/Domains/Clientes/Interfaces/IClienteRepository.cs
@@ -1,7 +1,7 @@
 ﻿using SistemaVendas.Core.Domains.Clientes.Entities;
+using SistemaVendas.Core.Shared.Entities;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SistemaVendas.Core.Domains.Clientes.Interfaces
@@ -9,10 +9,12 @@ namespace SistemaVendas.Core.Domains.Clientes.Interfaces
     public interface IClienteRepository
     {
         Task<int> Delete(Guid clienteId);
+        Task<PagedList<Cliente>> GetALL(ClienteParams clienteParams);
         Task<IEnumerable<Cliente>> GetAll();
         Task<Cliente> GetById(Guid clienteId);
         Task<int> Insert(Cliente cliente);
         Task<int> Save();
         Task<int> Update(Cliente cliente);
+        bool ExisteCliente(long cpf);
     }
 }
