1ec83ecfa6c0a02657b8b1f1b298b1583aae60f0
Ângelo Freitas Santos <ajkfreitas@gmail.com>
2020-03-11 19:49:30 -0300

    variaveis de ambite angular

--- SistemaVendas-front/SVendas/src/environments/environment.prod.ts
+++ SistemaVendas-front/SVendas/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiVendas: 'http://localhost:6001/svendas'
+  apiVendas: '/api-vendas/svendas'
 };
--- SistemaVendas-front/SVendas/src/environments/environment.sandbox.ts
+++ SistemaVendas-front/SVendas/src/environments/environment.sandbox.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiVendas: '/app/svendas'
+  apiVendas: '/api-vendas/svendas'
 };
